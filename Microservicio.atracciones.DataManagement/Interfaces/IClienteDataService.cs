113fa00d29ccd1b9457228c4f42ad512aa9e757c
xiHeredia <jiheredia@puce.edu.ec>
2026-04-29 01:39:45 -0500

    Update IClienteDataService.cs mejora

--- Microservicio.atracciones.DataManagement/Interfaces/IClienteDataService.cs
+++ Microservicio.atracciones.DataManagement/Interfaces/IClienteDataService.cs
@@ -11,6 +11,7 @@ namespace Microservicio.atracciones.DataManagement.Interfaces;
 public interface IClienteDataService
 {
     Task<ClienteDataModel?> ObtenerPorIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<ClienteDataModel?> ObtenerPorUsuarioIdAsync(int usuarioId, CancellationToken cancellationToken = default);
     Task<IReadOnlyList<ClienteDataModel>> ListarAsync(CancellationToken cancellationToken = default);
     Task<int> CrearAsync(ClienteDataModel model, CancellationToken cancellationToken = default);
     Task<bool> ActualizarAsync(ClienteDataModel model, CancellationToken cancellationToken = default);
